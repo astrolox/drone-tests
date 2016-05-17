eeae5cf92a6aff4777155c4763d16300e1cba6ee
James Poole <james.poole@fasthosts.co.uk>
2016-05-17 14:07:38 +0100

    Adjusted PHP version test.

--- shared/docker-ubuntu-16-apache-2.4-php-5.6/init.rb
+++ shared/docker-ubuntu-16-apache-2.4-php-5.6/init.rb
@@ -14,12 +14,12 @@ RSpec.shared_examples "docker-ubuntu-16-apache-2.4-php-5.6" do
       @container.start
     end
 
-    describe command("echo '<?php phpinfo(); ?>' > /var/www/html/rspecphpinfo.php") do
+    describe command("echo '<?php echo phpversion(); ?>' > /var/www/html/rspecphpversion.php") do
       its(:exit_status) { should eq 0 }
     end
 
-    describe command("curl -sS http://localhost:#{LISTEN_PORT}/rspecphpinfo.php") do
-      its(:stdout) { should match (/PHP Version 5\.6\.21\-7\+donate\.sury\.org~xenial\+1/) }
+    describe command("curl -sS http://localhost:#{LISTEN_PORT}/rspecphpversion.php") do
+      its(:stdout) { should match (/5\.6\.21\-7\+donate\.sury\.org~xenial\+1/) }
       its(:stderr) { should eq "" }
     end
 
