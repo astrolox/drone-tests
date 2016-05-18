82a2978e92f45a6fc6ff0ae66371c7691fd89920
James Eckersall <james.eckersall@fasthosts.com>
2016-05-18 15:49:43 +0100

    added loop to handle php test files

--- shared/docker-ubuntu-16-apache-2.4-php-5.6/init.rb
+++ shared/docker-ubuntu-16-apache-2.4-php-5.6/init.rb
@@ -12,17 +12,19 @@ RSpec.shared_examples "docker-ubuntu-16-apache-2.4-php-5.6" do
     )
     @container.start
 
-    describe command("echo '<?php echo phpversion(); ?>' > /var/www/html/rspecphpversion.php") do
-      its(:exit_status) { should eq 0 }
-    end
-
     describe command('id -u') do
       its(:stdout) { should match /^100000$/ }
     end
 
-    describe command("curl -sS http://localhost:#{LISTEN_PORT}/rspecphpversion.php") do
-      its(:stdout) { should start_with "5.6." }
-      its(:stderr) { should eq "" }
+    cwd=Pathname.new(File.join(File.dirname(__FILE__)))
+    files = Dir["#{cwd}/files/*.php"]
+    short_files = files.map { |f| File.basename(f) }
+    Specinfra::Runner.send_file( files, "/var/www/html/")
+    short_files.each do |f|
+      describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{f}") do
+        its(:stdout) { should eq "Success" }
+        its(:stderr) { should eq "" }
+      end
     end
 
     @container.kill
