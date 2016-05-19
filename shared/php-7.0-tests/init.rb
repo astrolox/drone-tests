d062f85e831c14ddc8bcbbb05377c5b243d1d7ea
James Poole <james.poole@fasthosts.co.uk>
2016-05-19 10:09:26 +0100

    Copied php5.6 tests for php7.0 folder.

---
+++ shared/php-7.0-tests/init.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require 'serverspec'
+
+RSpec.shared_examples "php-5.6-tests" do
+
+  describe "Container" do
+    @container = Docker::Container.create(
+      'Image'        => Docker::Image.get(ENV['IMAGE']).id,
+      'HostConfig'   => {
+      'PortBindings' => { "#{LISTEN_PORT}/tcp" => [{ 'HostPort' => "#{LISTEN_PORT}" }]}
+      }
+    )
+    @container.start
+
+    describe command('id -u') do
+      its(:stdout) { should match /^100000$/ }
+    end
+
+    cwd=Pathname.new(File.join(File.dirname(__FILE__)))
+    files = Dir["#{cwd}/files/*.php"]
+    short_files = files.map { |f| File.basename(f) }
+    Specinfra::Runner.send_file( files, "/var/www/html/")
+    short_files.each do |f|
+      describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{f}") do
+        its(:stdout) { should eq "Success" }
+        its(:stderr) { should eq "" }
+      end
+    end
+
+    @container.kill
+    @container.delete(:force => true)
+  end
+
+end
+
+
