260a64d66cc9aa3d5b250f6e98b3f55ecdc3941b
fhmarkh <mark.hawkins@fasthosts.com>
2016-05-23 14:26:43 +0100

    adding initial checks for phpMyAdmin

--- shared/phpmyadmin-4.6-tests/init.rb
+++ shared/phpmyadmin-4.6-tests/init.rb
@@ -3,8 +3,18 @@ require 'serverspec'
 
 RSpec.shared_examples "phpmyadmin-4.6-tests" do
 
-    # describe ... do
-    #
-    # end
+    describe command("curl -sS http://localhost:#{LISTEN_PORT}") do
+        its(:stdout) { should contain "phpMyAdmin" }
+        its(:stderr) { should eq "" }
+    end
 
+    describe command("echo $PHP_UPLOAD_MAX_FILESIZE") do
+        its(:stdout) { should contain "64M"}
+        its(:stderr) { should eq "" }
+    end
+
+    describe file('/hooks/supervisord-pre.d/40_phpmyadmin_config_secret') do
+        it { should exist }
+        it { should be_file }
+    end
 end
