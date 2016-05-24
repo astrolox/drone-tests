f97e6c2fcd3d5569ebbe2b51e4e7a0f166b46b8b
James Eckersall <james.eckersall@fasthosts.com>
2016-05-24 21:02:50 +0100

    minor tweak to php tests

--- shared/php-5.6-tests/init.rb
+++ shared/php-5.6-tests/init.rb
@@ -3,25 +3,20 @@ require 'serverspec'
 
 RSpec.shared_examples "php-5.6-tests" do
 
-  describe "Container" do
+  describe command('id -u') do
+    its(:stdout) { should match /^100000$/ }
+  end
 
-    describe command('id -u') do
-      its(:stdout) { should match /^100000$/ }
+  cwd=Pathname.new(File.join(File.dirname(__FILE__)))
+  files = Dir["#{cwd}/files/*.php"]
+  short_files = files.map { |f| File.basename(f) }
+  puts "Transferring files to container: #{short_files}"
+  Specinfra::Runner.send_file( files, "/var/www/html/")
+  short_files.each do |f|
+    describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{f}") do
+      its(:stdout) { should eq "Success" }
+      its(:stderr) { should eq "" }
     end
-
-    cwd=Pathname.new(File.join(File.dirname(__FILE__)))
-    files = Dir["#{cwd}/files/*.php"]
-    short_files = files.map { |f| File.basename(f) }
-    Specinfra::Runner.send_file( files, "/var/www/html/")
-    short_files.each do |f|
-      describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{f}") do
-        its(:stdout) { should eq "Success" }
-        its(:stderr) { should eq "" }
-      end
-    end
-
   end
 
 end
-
-
