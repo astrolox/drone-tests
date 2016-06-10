79ec2032c46ee8741763ae561a8a1a70d55b7249
James Poole <james.poole@fasthosts.com>
2016-06-10 10:04:27 +0100

    Added extra backend lines for further testing with mysql image.

--- shared/docker-ubuntu-16-nginx-1.10.0/init.rb
+++ shared/docker-ubuntu-16-nginx-1.10.0/init.rb
@@ -47,11 +47,13 @@ RSpec.shared_examples "docker-ubuntu-16-nginx-1.10.0" do
     it { should exist }
     it { should be_file }
   end
-
+  
   cwd=Pathname.new(File.join(File.dirname(__FILE__)))
   testfile = Dir["#{cwd}/files/test.html"]
   short_files = testfile.map { |f| File.basename(f) }
   puts "Transferring files to container: #{short_files}"
+#  set :backend, :docker
+#  set :docker_container, @container.id
   Specinfra::Runner.send_file( testfile, "/var/www/html/")
   short_files.each do |f|
     describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{f}") do
