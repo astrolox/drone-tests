137d7d4983c3ef4069920370ae05ffd8471bc153
fhmarkh <mark.hawkins@fasthosts.com>
2016-06-21 12:27:29 +0100

    Added another test for zend framework

--- shared/zend-2-tests/init.rb
+++ shared/zend-2-tests/init.rb
@@ -8,4 +8,9 @@ RSpec.shared_examples "zend-2-tests" do
       its(:stderr) { should eq "" }
     end
 
+    describe command("curl -sS http://localhost:#{LISTEN_PORT}/index.php") do
+      its(:stdout) { should contain  "Zend Framework version 2.5.1" }
+      its(:stderr) { should eq "" }
+    end
+
 end
