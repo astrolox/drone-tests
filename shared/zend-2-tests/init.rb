dabe0d4ee585aab91c20adccb75eb9ffc70b9aaf
fhmarkh <mark.hawkins@fasthosts.com>
2016-06-30 11:11:01 +0100

    Changing version number as pinning to most recent v2 stable image of skeleton app

--- shared/zend-2-tests/init.rb
+++ shared/zend-2-tests/init.rb
@@ -5,7 +5,7 @@ RSpec.shared_examples "zend-2-tests" do
 
     describe command("curl -sS http://localhost:#{LISTEN_PORT}/") do
       its(:stdout) { should contain  "ZF2 Skeleton Application" }
-      its(:stdout) { should contain  "Zend Framework version 2.5.1" }
+      its(:stdout) { should contain  "Zend Framework version 2.3.3" }
       its(:stderr) { should eq "" }
     end
 
