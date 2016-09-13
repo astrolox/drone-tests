9aee3e5853f5cbe3ede137cb6846d536b94a1dba
Nick Campion <nick.campion@fasthosts.com>
2016-09-13 15:50:28 +0100

    add line break to gem path check

--- shared/ruby-2.3-tests/init.rb
+++ shared/ruby-2.3-tests/init.rb
@@ -6,14 +6,14 @@ RSpec.shared_examples "ruby-2.3-tests" do
   describe command('id -u') do
     its(:stdout) { should match /^100000$/ }
   end
-  
+
   describe command("ruby --version") do
     its(:stdout) { should contain  "ruby 2.3" }
     its(:stderr) { should eq "" }
   end
-    
+
   describe command("echo $GEM_HOME") do
-    its(:stdout) { should eq  "/var/www/._gems" }
+    its(:stdout) { should eq  "/var/www/._gems\n" }
     its(:stderr) { should eq "" }
   end
 
