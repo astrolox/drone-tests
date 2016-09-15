767180356850894cd01cb0c3f805c15d5f2f83a2
Chris James <chris.james@fasthosts.com>
2016-09-15 10:11:12 +0100

    Fixing test scripts to actually output the issue in the event of failure

--- shared/mod_perl-tests/files/perl/modperl.pl
+++ shared/mod_perl-tests/files/perl/modperl.pl
@@ -1,6 +1,11 @@
 use strict;
-
-(exists {map { $_ => 1 } @INC}->{'/var/www/cpan/perl5'}) or die "Failed to find custom cpan library in \@INC\n";
-
 print "Content-type: text/plain\n\n";
-print "Success";
+
+if(exists {map { $_ => 1 } @INC}->{'/var/www/cpan/perl5'})
+{
+	print "Success";
+}
+else
+{
+	print "Failed to find custom cpan library in \@INC\n";
+}
