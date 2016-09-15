7d2d219ca299c399b1b12c43cd6a2597b6539e5f
Chris James <chris.james@fasthosts.com>
2016-09-15 09:51:40 +0100

    Fixing spec test for modperl template

--- shared/mod_perl-tests/init.rb
+++ shared/mod_perl-tests/init.rb
@@ -13,30 +13,27 @@ RSpec.shared_examples "mod_perl-tests" do
       it { should be_directory }
     end
 
-    puts "Getting CWD"
     cwd=Pathname.new(File.join(File.dirname(__FILE__)))
 
-    puts "CWD is #{cwd}"
-
-   # ['perl','cgi-bin','html'].each do |dir|
-   #   files = Dir["#{cwd}/files/#{dir}/*"]
-   #   short_files = files.map { |f| File.basename(f) }
-   #   puts "Transferring #{dir} files to container: #{short_files}"
-   #   Specinfra::Runner.send_file( files, "/var/www/#{dir}/")
-   #   puts "  Running #{dir} tests"
-   #   short_files.each do |f|
-   #     if dir == 'html'
-   #       test = f
-   #     else
-   #      test = "#{dir}/#{f}"
-   #    end
-   #    puts "    Testing #{test}"
-   #   describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{test}") do
-   #      its(:stdout) { should eq "Success" }
-   #      its(:stderr) { should eq "" }
-   #    end
-   #  end
-   # end
+    ['perl','cgi-bin','html'].each do |dir|
+      files = Dir["#{cwd}/files/#{dir}/*"]
+      short_files = files.map { |f| File.basename(f) }
+      puts "Transferring "#{short_files}" to directory /var/www/#{dir}"
+      Specinfra::Runner.send_file( files, "/var/www/#{dir}/")
+      puts "  Running #{dir} tests"
+      short_files.each do |f|
+        if dir == 'html'
+          test = f
+        else
+         test = "#{dir}/#{f}"
+       end
+       puts "    Testing #{test}"
+      describe command("curl -sS http://localhost:#{LISTEN_PORT}/#{test}") do
+         its(:stdout) { should eq "Success" }
+         its(:stderr) { should eq "" }
+       end
+     end
+    end
   end
 #end
 
