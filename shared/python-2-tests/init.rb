e8abaaa0f117ca3083c7ed66abde026f2adc4556
Brian Wilkinson <brian.wilkinson@fasthosts.com>
2016-09-21 15:45:13 +0100

    Merge fail - re-implemented failing test

--- shared/python-2-tests/init.rb
+++ shared/python-2-tests/init.rb
@@ -11,18 +11,5 @@ RSpec.shared_examples "python-2-tests" do
     its(:stdout) { should contain  "" }
     its(:stderr) { should contain  "Python 2" }
   end
-<<<<<<< 08fba85aaabbfbe3537ae3e2e2dc5fd651a52b9b
-
-  describe command("which python") do
-    its(:stdout) { should contain  "/var/www/._venv/bin/python" }
-    its(:stderr) { should eq "" }
-  end
-
-  describe command("which pip") do
-    its(:stdout) { should contain  "/var/www/._venv/bin/pip" }
-    its(:stderr) { should eq "" }
-  end
-=======
->>>>>>> Removed rspec test that expects virtual env to exist already. This wont happen any more because the environment gets setup when the pod runs for the first time.
 
 end
