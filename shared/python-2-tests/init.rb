19abfebd3f764920563212d0746c2bfd04b017f6
Brian Wilkinson <brian.wilkinson@fasthosts.com>
2016-09-21 14:47:33 +0100

    Removed rspec test that expects virtual env to exist already. This wont happen any more because the environment gets setup when the pod runs for the first time.

--- shared/python-2-tests/init.rb
+++ shared/python-2-tests/init.rb
@@ -11,6 +11,7 @@ RSpec.shared_examples "python-2-tests" do
     its(:stdout) { should contain  "" }
     its(:stderr) { should contain  "Python 2" }
   end
+<<<<<<< 08fba85aaabbfbe3537ae3e2e2dc5fd651a52b9b
 
   describe command("which python") do
     its(:stdout) { should contain  "/var/www/._venv/bin/python" }
@@ -21,5 +22,7 @@ RSpec.shared_examples "python-2-tests" do
     its(:stdout) { should contain  "/var/www/._venv/bin/pip" }
     its(:stderr) { should eq "" }
   end
+=======
+>>>>>>> Removed rspec test that expects virtual env to exist already. This wont happen any more because the environment gets setup when the pod runs for the first time.
 
 end
