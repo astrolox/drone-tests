019d7ac535968b282d56552a7fe24d78dc74a175
Nick Campion <nick.campion@fasthosts.com>
2016-10-14 18:03:49 +0100

    Revert "remove x-forward vars"
    This reverts commit 9535589174b59f6369ec3f50edd4ccd97bec0d3a.

--- shared/docker-ubuntu-16-apache-2.4/files/test/rpaf.sh
+++ shared/docker-ubuntu-16-apache-2.4/files/test/rpaf.sh
@@ -2,5 +2,5 @@
 printf "Content-type: text/html\n\n"
 printf "<br />\n"
 
-printf "Remote address: $REMOTE_ADDR<br />\n"
+printf "Server address: $SERVER_ADDR<br />\n"
 printf "Server Port: $SERVER_PORT<br />\n"
