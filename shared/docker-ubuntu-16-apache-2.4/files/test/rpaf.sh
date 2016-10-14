cf78731a75f1150c14db6d286da7eb8bb6a4245d
Nick Campion <nick.campion@fasthosts.com>
2016-10-14 17:49:59 +0100

    remove x-forward vars

--- shared/docker-ubuntu-16-apache-2.4/files/test/rpaf.sh
+++ shared/docker-ubuntu-16-apache-2.4/files/test/rpaf.sh
@@ -4,5 +4,3 @@ printf "<br />\n"
 
 printf "Server address: $SERVER_ADDR<br />\n"
 printf "Server Port: $SERVER_PORT<br />\n"
-printf "X-Forwarded-For: $HTTP_X_FORWARDED_FOR<br />\n"
-printf "X-Forwarded-Port: $HTTP_X_FORWARDED_PORT<br />\n"
