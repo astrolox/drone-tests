4e3d355689590b012764c4afb073bf5bad0868ce
Nick Campion <nick.campion@fasthosts.com>
2016-10-14 16:46:20 +0100

    fix tests

--- shared/docker-ubuntu-16-apache-2.4/init.rb
+++ shared/docker-ubuntu-16-apache-2.4/init.rb
@@ -52,8 +52,9 @@ RSpec.shared_examples "docker-ubuntu-16-apache-2.4" do
   cwd=Pathname.new(File.join(File.dirname(__FILE__)))
   files = Dir["#{cwd}/files/test"]
   Specinfra::Runner.send_file( files, "/var/www/html/")
-  describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\"") do
-    its(:stdout) { should contain "1.2.3.4" and "99"}
+  describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\" http://localhost:#{LISTEN_PORT}/test/rpaf.sh") do
+    its(:stdout) { should contain('1.2.3.4') }
+    its(:stdout) { should contain('99') }
     its(:stderr) { should eq "" }
   end
 
