43dc7b9ebbbca663c3af0dc530a771d712cad324
Nick Campion <nick.campion@fasthosts.com>
2016-10-17 09:19:47 +0100

    correct var

--- shared/docker-ubuntu-16-apache-2.4/files/test/rpaf.sh
+++ shared/docker-ubuntu-16-apache-2.4/files/test/rpaf.sh
@@ -2,7 +2,5 @@
 printf "Content-type: text/html\n\n"
 printf "<br />\n"
 
-printf "Remote address: $HTTP_REMOTE_ADDR<br />\n"
+printf "Remote address: $REMOTE_ADDR<br />\n"
 printf "Server Port: $SERVER_PORT<br />\n"
-#printf "X-Forwarded-For: $HTTP_X_FORWARDED_FOR<br />\n"
-#printf "X-Forwarded-Port: $HTTP_X_FORWARDED_PORT<br />\n"
