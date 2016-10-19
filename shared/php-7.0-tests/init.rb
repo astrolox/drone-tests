e0b6585db9521c478ca562ef2b302e25113fadb0
Nick Campion <nick.campion@fasthosts.com>
2016-10-19 15:44:20 +0100

    add tests for fastcgi params

--- shared/php-7.0-tests/init.rb
+++ shared/php-7.0-tests/init.rb
@@ -19,4 +19,14 @@ RSpec.shared_examples "php-7.0-tests" do
     end
   end
 
+  describe command("curl -sS -H \"X-Forwarded-For: 99.99.99.99\" -H \"X-Forwarded-Port: 99\" http://localhost:#{LISTEN_PORT}/phpinfo.php | grep \"REMOTE_ADDR\"") do
+    its(:stdout) { should contain "99.99.99.99" }
+    its(:stderr) { shold eq "" }
+  end
+
+  describe command("curl -sS -H \"X-Forwarded-For: 99.99.99.99\" -H \"X-Forwarded-Port: 99\" http://localhost:#{LISTEN_PORT}/phpinfo.php | grep \"SERVER_PORT\"") do
+    its(:stdout) { should contain "99" }
+    its(:stderr) { should eq "" }
+  end
+
 end
