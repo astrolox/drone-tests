d55ccc001c10c19310573f94089f1eefcbdea441
Nick Campion <nick.campion@fasthosts.com>
2016-10-20 10:12:47 +0100

    make tests work with apache rpaf

--- shared/php-5.6-tests/init.rb
+++ shared/php-5.6-tests/init.rb
@@ -22,12 +22,12 @@ RSpec.shared_examples "php-5.6-tests" do
   phpinfo = Dir["#{cwd}/phpinfo.php"]
   Specinfra::Runner.send_file( phpinfo, "/var/www/html/")
 
-  describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\" http://localhost:#{LISTEN_PORT}/phpinfo.php | grep \"REMOTE_ADDR\"") do
+  describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\" http://127.0.0.1:#{LISTEN_PORT}/phpinfo.php | grep \"REMOTE_ADDR\"") do
     its(:stdout) { should contain "1.2.3.4" }
     its(:stderr) { should eq "" }
   end
 
-  describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\" http://localhost:#{LISTEN_PORT}/phpinfo.php | grep \"SERVER_PORT\"") do
+  describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\" http://127.0.0.1:#{LISTEN_PORT}/phpinfo.php | grep \"SERVER_PORT\"") do
     its(:stdout) { should contain "99" }
     its(:stderr) { should eq "" }
   end
